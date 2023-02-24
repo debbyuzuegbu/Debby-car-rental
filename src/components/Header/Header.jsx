969e27bed4a54a96f28a2b581b516f1e10875a26
Deborah <debbyuzuegbu@gmail.com>
2023-02-24 05:02:14 +0100

    added header details

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
               <div className="header__top__left">
                 <span>Need Help?</span>
                 <span className="header__top__help">
-                  <i class="ri-phone-fill"></i> +1-202-555-0149
+                  <i class="ri-phone-fill"></i> +234-81156772263
                 </span>
               </div>
             </Col>
@@ -86,8 +86,8 @@ const Header = () => {
                   <i class="ri-earth-line"></i>
                 </span>
                 <div className="header__location-content">
-                  <h4>Bangladesh</h4>
-                  <h6>Sylhet City, Bangladesh</h6>
+                  <h4>Island</h4>
+                  <h6>Lagos City, Nigeria</h6>
                 </div>
               </div>
             </Col>
@@ -99,7 +99,7 @@ const Header = () => {
                 </span>
                 <div className="header__location-content">
                   <h4>Sunday to Friday</h4>
-                  <h6>10am - 7pm</h6>
+                  <h6>10am - 5pm</h6>
                 </div>
               </div>
             </Col>
