87d2b86761845ec0a00ea29d94657fdceedb611e
debbyuzuegbu <debbyuzuegbu@gmail.com>
2026-04-24 03:31:07 +0100

    fix

--- src/assets/data/blogData.js
+++ src/assets/data/blogData.js
@@ -2,7 +2,7 @@
 import img01 from "../all-images/blog-img/blog-1.jpg";
 import img02 from "../all-images/blog-img/blog-2.jpg";
 import img03 from "../all-images/blog-img/blog-3.jpg";
-
+//
 const blogData = [
   {
     id: 1,
